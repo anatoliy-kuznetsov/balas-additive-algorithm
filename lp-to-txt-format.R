fe06102c09d54312e6c5a0c5601502b6d253afd2
Selin Bayramoglu <43606218+SelinBayramoglu@users.noreply.github.com>
2022-04-07 19:52:19 -0400

    Update lp-to-txt-format.R

--- lp-to-txt-format.R
+++ lp-to-txt-format.R
@@ -1,22 +1,113 @@
-# Install package to read in problems in CPLEX's LP format
 #install.packages("Rglpk")
-
 library("Rglpk")
 
-# Set working directory
 setwd("/Users/selinbayramoglu/Desktop/ComputationalMethods/Program")
 
-problem <- Rglpk_read_file("small-example.lp", type="CPLEX_LP")
+#args <- commandArgs()
+#probname = args[7]
 
-nvar = problem$objective$nrow   # number of variables
+probname = "MPS-format/academictimetablebig.mps"
+problem <- Rglpk_read_file(probname, type="MPS_free")
 
-ncons = length(problem$constraints[[3]])    # number of rows
 
-nnz = length(problem$constraints[[1]]$v)  # number of nonzero entries in A
+# ========= Transform into Balas form if needed ======
 
-obj_nnz = length(problem$objective$i)   # number of nonzero entries in c
+offset = 0
 
-rhs = problem$constraints[[3]]    # rhs vector b
+nvar = problem$constraints[[1]]$ncol
+
+ncons = problem$constraints[[1]]$nrow
+
+nnz = length(problem$constraints[[1]]$v)
+
+obj_nnz = length(problem$objective$i)
+
+
+
+# Variable transformation
+if (obj_nnz > 0) {
+  for( n in 1:obj_nnz)
+  {
+    if( problem$objective$v[n] < 0 )
+    {
+      myvar = problem$objective$i
+      # varlist = c(varlist, myvar)
+      offset = offset + problem$objective$v[n]
+      problem$objective$v[n] = -1 * problem$objective$v[n] # make cj >= 0
+      
+      locations = which(problem$constraints[[1]]$j == n) 
+      rowindices = problem$constraints[[1]]$i[locations]
+      
+      problem$constraints[[3]][rowindices] = problem$constraints[[3]][rowindices] - problem$constraints[[1]]$v[locations] # update rhs
+      problem$constraints[[1]]$v[locations] = -1 * problem$constraints[[1]]$v[locations] # update A.j (A.j *= -1)
+    }
+  }
+}
+
+
+# Transformation of ">=" constraints
+for( m in 1:ncons )
+{
+  if(problem$constraints[[2]][m] == ">=") {
+    # conslist1 = c(conslist1, m)
+    
+    problem$constraints[[2]][m] = "<="
+    
+    locations = which(problem$constraints[[1]]$i == m)
+    
+    problem$constraints[[3]][m] = -1 * problem$constraints[[3]][m]
+    problem$constraints[[1]]$v[locations] = -1 * problem$constraints[[1]]$v[locations]
+  }
+}
+
+# Transformation of "=" constraints
+
+ctr = 0
+
+for( m in 1:ncons )
+{
+  if(problem$constraints[[2]][m] == "==") {
+    # conslist2 = c(conslist2, m)
+    ctr = ctr + 1
+    problem$constraints[[2]][m] = "<="
+    
+    locations = which(problem$constraints[[1]]$i == m)
+    start_loc = which(problem$constraints[[1]]$i == m)[1]
+    
+    problem$constraints[[2]] = c(problem$constraints[[2]], "<=") # append new constraint at the bottom of A
+    
+    nlocations = sum( problem$constraints[[1]]$i == m )
+    locations = which( problem$constraints[[1]]$i == m )
+    
+    if(nlocations > 0) {
+      problem$constraints[[1]]$i = c( problem$constraints[[1]]$i, rep(0, nlocations) ) #extend vector for new row inds
+      problem$constraints[[1]]$j = c( problem$constraints[[1]]$j, rep(0, nlocations) ) #extend vector for new col inds
+      problem$constraints[[1]]$v = c( problem$constraints[[1]]$v, rep(0, nlocations) ) #extend vector for new nonzero values
+      
+      problem$constraints[[3]] = c(problem$constraints[[3]],-1 * problem$constraints[[3]][m]) # append new rhs to the end
+      problem$constraints[[1]]$i[nnz + 1:nlocations] = rep(ncons + ctr, nlocations)
+      problem$constraints[[1]]$j[nnz + 1:nlocations] = problem$constraints[[1]]$j[locations]
+      problem$constraints[[1]]$v[nnz + 1:nlocations] = -1 * problem$constraints[[1]]$v[locations]
+      nnz = nnz + nlocations # update number of nonzeros, i.e. the lengths of i,j and v vectors
+    }
+  }
+}
+
+
+
+# ======== End transform =======
+
+
+
+nvar = problem$constraints[[1]]$ncol
+
+ncons = problem$constraints[[1]]$nrow + ctr
+
+nnz = length(problem$constraints[[1]]$v)
+
+obj_nnz = length(problem$objective$i)
+
+rhs = problem$constraints[[3]]
 
 mymatA = rbind(problem$constraints[[1]]$i, problem$constraints[[1]]$j, problem$constraints[[1]]$v)
 
@@ -26,15 +117,15 @@ myc = rbind(problem$objective$i, problem$objective$v)
 
 newc = as.vector(myc)
 
-# Create file
-cat(nvar, file="outfile.txt",sep="\n")
-cat(ncons, file="outfile.txt", sep="\n", append = "TRUE")
-cat(nnz, file="outfile.txt", sep="\n", append = "TRUE")
-cat(obj_nnz, file="outfile.txt", sep="\n", append = "TRUE")
-cat(newc, file="outfile.txt", append = "TRUE") ##nonzero objective coefficients
-cat(file="outfile.txt", sep="\n", append = "TRUE")
-cat(rhs,file="outfile.txt", append = "TRUE") ##rhs
-cat(file="outfile.txt", sep="\n", append = "TRUE")
-cat(newmatA,file="outfile.txt", append = "TRUE")
+outfile = paste(probname,".txt", sep="")
 
+cat(nvar, file = outfile, sep="\n")
+cat(ncons, file = outfile, sep="\n", append = "TRUE")
+cat(nnz, file = outfile, sep="\n", append = "TRUE")
+cat(obj_nnz, file = outfile, sep="\n", append = "TRUE")
+cat(newc, file = outfile, append = "TRUE") ##nonzero objective coefficients
+cat(file = outfile, sep="\n", append = "TRUE")
+cat(rhs, file = outfile, append = "TRUE") ##rhs
+cat(file = outfile, sep="\n", append = "TRUE")
+cat(newmatA, file = outfile, append = "TRUE")
 
